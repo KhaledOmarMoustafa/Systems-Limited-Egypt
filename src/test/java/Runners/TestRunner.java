28e1b5dd93e88c6333864dec0d769a1aea4e07df
khaled <55811132+KhaledOmarMoustafa@users.noreply.github.com>
2024-11-18 22:45:21 +0200

    Update TestRunner.java

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -4,10 +4,10 @@ import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 @CucumberOptions(
-        features = "src/test/resources/features/OpenBrowser.feature", // Path to your feature files
+        features = "src/test/resources/features/OpenBrowser.feature", // Path to  feature files
         glue = "StepDefinition",// Package where step definitions are located
         tags ="@regression",
-        plugin = {"pretty", "html:target/cucumber-reports"}  // Optional: specify report options
+        plugin = {"pretty", "html:target/cucumber-reports"}  
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 
